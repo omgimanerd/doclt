54599c72436d2e6090c2ace6425b085e07147b87
omgimanerd <alvin.lin.dev@gmail.com>
2017-01-09 11:51:21 -0500

    Decreased wrap size to 72 characters

--- docli.js
+++ docli.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 
+// Importing colors here will allow us to use it everywhere else since it
+// extends String.prototype.
+var colors = require('colors');
 var yargs = require('yargs');
 
 yargs.commandDir('commands')
   .demandCommand(1)
   .epilogue('See \'docli help <command\' for more information.')
-  .help()
   .recommendCommands()
   .version(() => require('./package.json').version)
-  .wrap(80)
+  .wrap(72)
   .argv;
