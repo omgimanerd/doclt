ca0c0bb65292503ff44a3d65c91f1935ccc744e7
omgimanerd <alvin.lin.dev@gmail.com>
2017-01-09 21:45:12 -0500

    Implemented domain record delete command

---
+++ commands/domains/records/delete.js
@@ -0,0 +1,23 @@
+/**
+ * @fileoverview Module handling the domain record delete command.
+ * @author alvin@omgimanerd.tech (Alvin Lin)
+ */
+
+exports.command = 'delete <domain> <record id>';
+
+exports.aliases = ['remove', 'del', 'rm'];
+
+exports.description = 'Delete a domain record'.yellow;
+
+exports.handler = (argv) => {
+  var digitalocean = require('digitalocean');
+
+  var token = require('../../../lib/token');
+  var util = require('../../../lib/util');
+  var client = digitalocean.client(token.get());
+
+  client.domains.deleteRecord(argv.domain, argv.recordid, (error) => {
+    util.handleError(error);
+    console.log('Domain record deleted.'.red);
+  });
+};
