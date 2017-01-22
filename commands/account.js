e525eb6031c24f2bb5545cac436055f44e9a105d
omgimanerd <alvin.lin.dev@gmail.com>
2017-01-21 21:13:13 -0500

    Implemented account information command

---
+++ commands/account.js
@@ -0,0 +1,43 @@
+/**
+ * @fileoverview Module handling the account detail command.
+ * @author alvin@omgimanerd.tech (Alvin Lin)
+ */
+
+var util = require('../lib/util');
+
+exports.command = 'account';
+
+exports.aliases = ['acc'];
+
+exports.description = 'Display account information'.yellow;
+
+exports.builder = (yargs) => {
+  util.globalConfig(yargs, exports.command);
+};
+
+exports.handler = (argv) => {
+  var client = util.getClient();
+
+  client.account.get((error, account) => {
+    util.handleError(error, argv.json);
+    if (argv.json) {
+      console.log(account);
+    } else {
+      var Table = require('cli-table2');
+      var table = new Table();
+      table.push([{
+        colSpan: 2,
+        content: 'UUID: '.red + account.uuid.toString().bold.cyan
+      }]);
+      table.push.apply(table, [
+        ['Status', util.colorAccountStatus(account.status)],
+        ['Status message', account.status_message || 'none'],
+        ['Email', account.email],
+        ['Email verified', account.email_verified ? 'yes'.green : 'no'.red],
+        ['Droplet Limit', account.droplet_limit],
+        ['Floating IP Limit', account.floating_ip_limit]
+      ].map((row) => [row[0].red, row[1]]));
+      console.log(table.toString());
+    }
+  });
+};
