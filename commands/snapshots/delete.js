192beb06b4f34ed592c0ae2cb90dc3a4fec53f5e
omgimanerd <alvin.lin.dev@gmail.com>
2017-01-26 14:16:42 -0500

    Implemented snapshot delete command

---
+++ commands/snapshots/delete.js
@@ -0,0 +1,25 @@
+/**
+ * @fileoverview Module handling the snapshot delete command.
+ * @author alvin@omgimanerd.tech (Alvin Lin)
+ */
+
+var Display = require('../../lib/Display');
+var Util = require('../../lib/Util');
+
+exports.command = 'delete <snapshot id>';
+
+exports.aliases = ['remove', 'del', 'rm'];
+
+exports.description = 'Delete a snapshot'.yellow;
+
+exports.builder = (yargs) => {
+  Util.globalConfig(yargs, 1, exports.command);
+};
+
+exports.handler = (argv) => {
+  var client = Util.getClient();
+  client.snapshots.delete(argv.snapshotid, (error) => {
+    Util.handleError(error);
+    Display.displayMessage('Snapshot deleted.');
+  });
+};
