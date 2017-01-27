9d1f34c1283271585ab1f5ec0c4d0ed01d7accfb
omgimanerd <alvin.lin.dev@gmail.com>
2017-01-27 17:11:52 -0500

    Fixed tag display and implemented untagging of resources

--- commands/tags/untag.js
+++ commands/tags/untag.js
@@ -1,16 +1,14 @@
 /**
- * @fileoverview Module handling the tagging of a resource.
+ * @fileoverview Module handling the untagging of a resource.
  * @author alvin@omgimanerd.tech (Alvin Lin)
  */
 
 var Display = require('../../lib/Display');
 var Util = require('../../lib/Util');
 
-exports.command = 'tag <resource type> <resource id> <tag>';
+exports.command = 'untag <resource type> <resource id> <tag>';
 
-exports.aliases = ['apply'];
-
-exports.description = 'Tag a resource'.yellow;
+exports.description = 'Untag a resource'.yellow;
 
 exports.builder = (yargs) => {
   Util.globalConfig(yargs, 2, exports.command);
@@ -18,11 +16,11 @@ exports.builder = (yargs) => {
 
 exports.handler = (argv) => {
   var client = Util.getClient();
-  client.tags.tag(argv.tag, [{
+  client.tags.untag(argv.tag, [{
     resource_type: argv.resourcetype,
     resource_id: argv.resourceid
   }], (error) => {
     Util.handleError(error);
-    Display.displayMessage('Resource tagged.');
+    Display.displayMessage('Resource untagged.');
   });
 };
