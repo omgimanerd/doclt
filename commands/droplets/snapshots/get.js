d1a468d510787174a6c4ccf55785b452fa514daa
omgimanerd <alvin.lin.dev@gmail.com>
2017-01-28 18:04:13 -0500

    Added redundant snapshots get under droplets

---
+++ commands/droplets/snapshots/get.js
@@ -0,0 +1,30 @@
+/**
+ * @fileoverview Module handling the snapshot getting command.
+ * @author alvin@omgimanerd.tech (Alvin Lin)
+ */
+
+var Display = require('../../../lib/Display');
+var Util = require('../../../lib/Util');
+
+exports.command = 'get <snapshot id>';
+
+exports.aliases = ['i', 'info'];
+
+exports.description = 'Info about a snapshot'.yellow;
+
+exports.builder = (yargs) => {
+  Util.globalConfig(yargs, 3, exports.command);
+};
+
+exports.handler = (argv) => {
+  var client = Util.getClient();
+  /**
+   * We're going to use the images endpoint to get the
+   * snapshot so that we can get information like
+   * distribution and type.
+   */
+  client.images.get(argv.snapshotid, (error, image) => {
+    Util.handleError(error);
+    Display.displayImage(image);
+  });
+};
