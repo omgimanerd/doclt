3a0dd36d70d0bea6b4a2f1e797ceaa0067b5257d
omgimanerd <alvin.lin.dev@gmail.com>
2017-01-28 22:31:00 -0500

    Implemented droplet password reset

---
+++ commands/droplets/password_reset.js
@@ -0,0 +1,25 @@
+/**
+ * @fileoverview Module handling the droplet password reset command.
+ * @author alvin@omgimanerd.tech (Alvin Lin)
+ */
+
+var Display = require('../../lib/Display');
+var Util = require('../../lib/Util');
+
+exports.command = 'password_reset <droplet id>';
+
+exports.aliases = ['password', 'pw'];
+
+exports.description = 'Reset the password on a droplet'.yellow;
+
+exports.builder = (yargs) => {
+  Util.globalConfig(yargs, 2, exports.command);
+};
+
+exports.handler = (argv) => {
+  var client = Util.getClient();
+  client.droplets.passwordReset(argv.dropletid, (error, action) => {
+    Util.handleError(error);
+    Display.displayActionID(action, 'Resetting droplet password.');
+  });
+};
