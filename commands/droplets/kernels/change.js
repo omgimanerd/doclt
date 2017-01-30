69fb4449a08e70990edfb4603549718a90e89588
omgimanerd <alvin.lin.dev@gmail.com>
2017-01-30 18:26:16 -0500

    Implemented droplet kernel change command

---
+++ commands/droplets/kernels/change.js
@@ -0,0 +1,25 @@
+/**
+ * @fileoverview Module handling the kernel changing command.
+ * @author alvin@omgimanerd.tech (Alvin Lin)
+ */
+
+var Display = require('../../../lib/Display');
+var Util = require('../../../lib/Util');
+
+exports.command = 'change <droplet id> <kernel id>';
+
+exports.description = 'Change the kernel of a droplet'.yellow;
+
+exports.builder = (yargs) => {
+  Util.globalConfig(yargs, 3, exports.command);
+};
+
+exports.handler = (argv) => {
+  var client = Util.getClient();
+  var dropletid = argv.dropletid;
+  var kernelid = argv.kernelid;
+  client.droplets.changeKernel(dropletid, kernelid, (error, action) => {
+    Util.handleError(error);
+    Display.displayActionID(action);
+  });
+};
