071b3e8082b2b7644a48c8a1da923e4c9bdb9d27
omgimanerd <alvin.lin.dev@gmail.com>
2017-01-30 18:22:12 -0500

    Moved kernels to kernels subcommand as list

--- commands/droplets/kernels.js
+++ commands/droplets/kernels.js
@@ -1,23 +1,17 @@
 /**
- * @fileoverview Module handling the kernels listing command.
+ * @fileoverview Module handling the droplet kernels subcommands.
  * @author alvin@omgimanerd.tech (Alvin Lin)
  */
 
-var Display = require('../../lib/Display');
 var Util = require('../../lib/Util');
 
-exports.command = 'kernels <droplet id>';
+exports.command = 'kernels';
 
-exports.description = 'List kernels of a droplet'.yellow;
+exports.aliases = ['kernel'];
+
+exports.description = 'List and change droplet kernels'.yellow;
 
 exports.builder = (yargs) => {
-  Util.globalConfig(yargs, 2, exports.command);
-};
-
-exports.handler = (argv) => {
-  var client = Util.getClient();
-  client.droplets.kernels(argv.dropletid, (error, kernels) => {
-    Util.handleError(error);
-    Display.displayKernels(kernels);
-  });
+  yargs.commandDir('kernels');
+  Util.globalConfig(yargs, 2, exports.command, true);
 };
---
+++ commands/droplets/kernels/list.js
@@ -0,0 +1,23 @@
+/**
+ * @fileoverview Module handling the kernels listing command.
+ * @author alvin@omgimanerd.tech (Alvin Lin)
+ */
+
+var Display = require('../../../lib/Display');
+var Util = require('../../../lib/Util');
+
+exports.command = 'list <droplet id>';
+
+exports.description = 'List kernels of a droplet'.yellow;
+
+exports.builder = (yargs) => {
+  Util.globalConfig(yargs, 3, exports.command);
+};
+
+exports.handler = (argv) => {
+  var client = Util.getClient();
+  client.droplets.kernels(argv.dropletid, (error, kernels) => {
+    Util.handleError(error);
+    Display.displayKernels(kernels);
+  });
+};
