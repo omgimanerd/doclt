08546a52ddf2e95ceaacd1e0d797668af130a975
omgimanerd <alvin.lin.dev@gmail.com>
2017-01-31 14:52:07 -0500

    Implemented robust error display

--- docli.js
+++ docli.js
@@ -11,10 +11,10 @@ var yargs = require('yargs');
 
 var Util = require('./lib/Util');
 
-yargs.commandDir('commands');
+yargs.commandDir('commands').fail(Util.handleFail);
 Util.globalConfig(yargs, 0, '', true);
-var argv = yargs.argv;
 
-if (argv.color) {
+var argv = yargs.argv;
+if (argv !== null && argv.color) {
   console.log('Invoking --color does nothing you idiot!'.bold.red.bgWhite);
 }
