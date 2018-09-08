46b56b5d69d74e0b49e0853fb4c5e539d949a07b
omgimanerd <alvin@omgimanerd.tech>
2018-09-08 04:23:01 -0400

    Implemented load balancer get subcommand

---
+++ commands/load-balancers.js
@@ -0,0 +1,18 @@
+/**
+ * @fileoverview Module handling the load balancer subcommands.
+ * @author alvin@omgimanerd.tech (Alvin Lin)
+ */
+
+const util = require('../lib/util')
+
+exports.command = 'load-balancers'
+
+exports.aliases = ['load-balancer', 'loadbalancers', 'loadbalancer', 'lb']
+
+exports.description = 'Create, delete, and manage load balancers.'.yellow
+
+exports.builder = yargs => {
+  yargs.commandDir('load-balancers')
+    .demandCommand()
+  util.globalConfig(yargs)
+}
---
+++ commands/load-balancers/get.js
@@ -0,0 +1,28 @@
+/**
+ * @fileoverview Module handling the load balancer getting command.
+ * @author alvin@omgimanerd.tech (Alvin Lin)
+ */
+
+const display = require('../../lib/display')
+const util = require('../../lib/util')
+
+exports.command = 'get <load balancer id>'
+
+exports.aliases = ['i', 'info']
+
+exports.description = 'Info about a load balancer'.yellow
+
+exports.builder = yargs => {
+  yargs.option('rules', {
+    description: 'Show the forwarding rules'.yellow
+  }).group(['rules'], 'Load Balancer Display Options:')
+  util.globalConfig(yargs)
+}
+
+exports.handler = argv => {
+  const client = util.getClient()
+  client.loadBalancers.get(argv.loadbalancerid, (error, loadbalancer) => {
+    util.handleError(error)
+    display.displayLoadBalancer(loadbalancer)
+  })
+}
