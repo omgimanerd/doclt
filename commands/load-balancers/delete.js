ca1eb5c0fab17d6d14937f2209e7ed2d5ec63e6c
omgimanerd <alvin@omgimanerd.tech>
2018-09-10 18:23:29 -0400

    Implemented load balancer delete command

---
+++ commands/load-balancers/delete.js
@@ -0,0 +1,24 @@
+/**
+ * @fileoverview Module handling the load balancer delete command.
+ * @author alvin@omgimanerd.tech (Alvin Lin)
+ */
+
+const display = require('../../lib/display')
+const util = require('../../lib/util')
+
+exports.command = 'delete <load balancer id>'
+
+exports.aliases = ['remove', 'del', 'rm']
+
+exports.description = 'Delete a load balancer'.yellow
+
+exports.builder = util.globalConfig
+
+exports.handler = argv => {
+  const client = util.getClient()
+  const id = argv.loadbalancerid
+  client.loadBalancers.delete(id, error => {
+    util.handleError(error)
+    display.displayMessage(`Load balancer ${id} deleted.`)
+  })
+}
