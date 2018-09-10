7f25e8fd92b5f925b8331335c57372724e73d474
omgimanerd <alvin@omgimanerd.tech>
2018-09-10 18:44:58 -0400

    Implemented droplet add command for load balancers

---
+++ commands/load-balancers/add-droplet.js
@@ -0,0 +1,31 @@
+/**
+ * @fileoverview Module handling the load balancer droplet add command.
+ * @author alvin@omgimanerd.tech (Alvin Lin)
+ */
+
+const display = require('../../lib/display')
+const util = require('../../lib/util')
+
+exports.command = 'add-droplet <load balancer id> <droplet ids>'
+
+exports.description = 'Add droplets to a load balancer'.yellow
+
+exports.builder = yargs => {
+  yargs.example(
+    '$0 load-balancers add-droplet 12345 52232,43451,32552')
+  util.globalConfig(yargs)
+}
+
+exports.handler = argv => {
+  const client = util.getClient()
+  const lbId = argv.loadbalancerid
+  const dropletIds = util.csvToArray(argv.dropletids)
+  client.loadBalancers.add(lbId, dropletIds, error => {
+    util.handleError(error)
+    const s = dropletIds.length > 0 ? 's' : ''
+    const dropletString = dropletIds.join(', ')
+    display.displayMessage(
+      `Droplet${s} ${dropletString} added to load balancer ${lbId}`
+    )
+  })
+}
