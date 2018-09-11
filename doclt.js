55d5f28514536cb75edc1ac80b3d4a7b8abeff4f
omgimanerd <alvin@omgimanerd.tech>
2018-09-11 13:16:02 -0400

    Reimplement error handling

--- doclt.js
+++ doclt.js
@@ -6,29 +6,33 @@
 
 const yargs = require('yargs')
 
+const util = require('./lib/util')
+
 const VERSION = require('./package.json').version
 
-yargs
-  .commandDir('commands')
-  .completion('completion', 'Generate bash completion script'.yellow)
-  .demandCommand()
-  .example('$0 completion >> .bashrc', 'Append bash completion script'.yellow)
-  .help('help', 'Show help for a command'.yellow)
-  .option('dev', {
-    hidden: true,
-    'boolean': true
-  })
-  .option('json', {
-    description: 'Output results as JSON'.yellow,
-    'boolean': true
-  })
-  .option('no-color', {
-    description: 'Disable colors'.yellow,
-    'boolean': true
-  })
-  .recommendCommands()
-  .scriptName('doclt')
-  .strict()
-  .version('version', 'Show version number'.yellow, VERSION)
-  .wrap(yargs.terminalWidth())
-  .parse()
+try {
+  yargs.commandDir('commands')
+    .completion('completion', 'Generate bash completion script'.yellow)
+    .demandCommand()
+    .help('help', 'Show help for a command'.yellow)
+    .option('dev', {
+      hidden: true,
+      'boolean': true
+    })
+    .option('json', {
+      description: 'Output results as JSON'.yellow,
+      'boolean': true
+    })
+    .option('no-color', {
+      description: 'Disable colors'.yellow,
+      'boolean': true
+    })
+    .recommendCommands()
+    .scriptName('doclt')
+    .strict()
+    .version('version', 'Show version number'.yellow, VERSION)
+    .wrap(yargs.terminalWidth())
+    .parse()
+} catch (error) {
+  util.handleFail(null, error)
+}
