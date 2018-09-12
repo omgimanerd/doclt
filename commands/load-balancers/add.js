fbdd35620dc60e6fdaf7c47ae4f8ca562a284b15
omgimanerd <alvin@omgimanerd.tech>
2018-09-12 02:01:44 -0400

    Implemented load balancer creation command

--- commands/load-balancers/add.js
+++ commands/load-balancers/add.js
@@ -3,12 +3,8 @@
  * @author alvin@omgimanerd.tech (Alvin Lin)
  */
 
-const prompt = require('prompt')
-
 const display = require('../../lib/display')
 const util = require('../../lib/util')
-
-const ALGORITHMS = ['round_robin', 'least_connections']
 
 exports.command = 'add'
 
@@ -17,49 +13,47 @@ exports.aliases = ['create']
 exports.description = 'Create a new load balancer'.yellow
 
 exports.builder = yargs => {
+  const command = '$0 load-balancers add'
   yargs.option('name', {
-    description: 'Set the load balancer name'.yellow
+    description: 'Set the load balancer name'.yellow,
+    required: true
   }).option('algorithm', {
     description: 'Set the load balancing algorithm'.yellow,
-    choices: ALGORITHMS
+    choices: ['round_robin', 'least_connections']
   }).option('region', {
-    description: 'Set the region (slug identifier)'.yellow
+    description: 'Set the region'.yellow,
+    required: true
   }).option('forwarding_rules', {
-    description: 'Set the forwarding rules'.yellow,
+    description: 'Set the forwarding rules (config object)'.yellow,
+    required: true,
     array: true
+  }).option('health_check', {
+    description: 'Set health check configuration (config object)'.yellow
+  }).option('sticky_sessions', {
+    description: 'Set stick session configuration (config object)'.yellow
   }).option('redirect_http_to_https', {
     description: 'Redirect HTTP requests to HTTPS'.yellow,
     'boolean': true
   }).option('droplet_ids', {
     description:
-      'Assign droplets to the load balancer (space separated)'.yellow,
+      'Assign droplets to the load balancer'.yellow,
     array: true
-  })
+  }).group([
+    'name', 'algorithm', 'region', 'forwarding_rules', 'health_check',
+    'sticky_sessions', 'redirect_http_to_https', 'droplet_ids'
+  ], 'Load Balancer Options:')
+    .example(`${command} \\
+    --name lb1 \\
+    --region nyc1 \\
+    --forwarding_rules \\
+    entry_protocol:tcp,entry_port:200,target_protocol:tcp,target_port:200 \\
+    --droplet_ids 1234 5677`)
 }
 
 exports.handler = argv => {
   const client = util.getClient()
-  prompt.message = ''
-  prompt.override = argv
-  prompt.start()
-  prompt.get({
-    properties: {
-      name: {
-        description: 'Load balancer name'.yellow,
-        required: true
-      },
-      algorithm: {
-        description: 'Load balancing algorithm (optional)'.yellow,
-        'default': 'round_robin',
-        conform: value => ALGORITHMS.indexOf(value) !== -1,
-        message: 'Algorithm must be round_robin or least_connections'
-      },
-      region: {
-        description: 'Region ID'.yellow,
-        required: true
-      },
-    }
-  }, (error, result) => {
-    console.log(result)
+  client.loadBalancers.create(argv, (error, loadBalancer) => {
+    util.handleError(error)
+    display.displayLoadBalancer(loadBalancer)
   })
 }
